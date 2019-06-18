bae58e73ab1f27d78699960eaeadd4acbfd6c3f8
Marcus <zongolomama@gmail.com>
2019-06-18 15:56:32 +0200

    SITE KILLA 1

--- 1/scriptsitekilla.js
+++ 1/scriptsitekilla.js
@@ -1,13 +1,12 @@
 
 $( document ).ready(function() {
 	alert("bienvenue");
-	$( "p" ).on( "click", function() {
+	$( "li" ).on( "click", function() {
 		if ($(this).hasClass('red')) {
 			$(this).removeClass('red'); 
 		}else{	
 			$(this).addClass('red');
-        }  		
-         $(this).html("FLEEKAH"); 
+        }  		  
     });
 });
 
