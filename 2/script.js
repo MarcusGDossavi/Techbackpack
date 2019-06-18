cd12affe267883863e404866411515ce4d5d7a97
Marcus <zongolomama@gmail.com>
2019-06-18 15:25:03 +0200

    Etape 3 Javascript

--- 2/script.js
+++ 2/script.js
@@ -1,5 +1,12 @@
 
 $( document ).ready(function() {
 	alert("bienvenue");
-	$("p").addClass('red');
+	$( "p" ).on( "click", function() {
+		if ($("p").hasClass('red')) {
+			$("p").removeClass('red'); 
+		}else{	
+			$("p").addClass('red');
+        }  		  
+    });
 });
+
